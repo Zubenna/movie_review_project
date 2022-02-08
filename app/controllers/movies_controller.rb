badaa05c7582f3a7ab31add074ece9c3b85cf9bb
zubenna <emelunna@gmail.com>
2022-02-08 10:21:36 +0100

    Added Star Rating and Average Rating Features

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,11 @@ class MoviesController < ApplicationController
 
     def show
         @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+        if @reviews.blank?
+          @avg_review = 0
+        else
+          @avg_review = @reviews.average(:rating).round
+       end
     end
 
     def new 
